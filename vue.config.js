08150fe1f93ca792101b0dabf639116b189280b3
laybrid <664531349@qq.com>
2025-04-21 22:12:21 +0800

    fix build bug

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  parallel: false,
   chainWebpack: (config) => {
     config.module.rule('md')
         .test(/\.md/)
